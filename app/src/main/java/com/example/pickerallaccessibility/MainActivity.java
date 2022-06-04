61db596dd81ea9158de9cb00ccfa44b2860fa826
Thomas Gransden <thomas.gransden@slimmingworld.co.uk>
2022-06-04 18:20:50 +0100

    Updates for demo

--- app/src/main/java/com/example/pickerallaccessibility/MainActivity.java
+++ app/src/main/java/com/example/pickerallaccessibility/MainActivity.java
@@ -17,6 +17,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -24,111 +26,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NumberPicker p = findViewById(R.id.picker);
-        p.setMinValue(0);
-        p.setMaxValue(10);
-        p.setWrapSelectorWheel(false);
+        final String[] focussedThing = {"", ""};
 
-        EditText mSelectedText = p.findViewById(Resources.getSystem().getIdentifier("numberpicker_input", "id", "android"));
+        NumberPicker p1 = findViewById(R.id.picker1);
+        p1.setDisplayedValues(new String[]{"0", "1","2", "3","4"});
+        p1.setMinValue(0);
+        p1.setMaxValue(4);
+        p1.setWrapSelectorWheel(false);
 
-        mSelectedText.setEnabled(false);
-        mSelectedText.setFocusable(false);
-        mSelectedText.setImeOptions(EditorInfo.IME_ACTION_NONE);
-        mSelectedText.setAccessibilityDelegate(new View.AccessibilityDelegate(){
+        EditText mSelectedText1 = p1.findViewById(Resources.getSystem().getIdentifier("numberpicker_input", "id", "android"));
+        mSelectedText1.setEnabled(false);
+        mSelectedText1.setFocusable(false);
+        mSelectedText1.setImeOptions(EditorInfo.IME_ACTION_NONE);
+        mSelectedText1.setAccessibilityDelegate(new View.AccessibilityDelegate(){
             @Override
             public void onInitializeAccessibilityNodeInfo(View host, AccessibilityNodeInfo info) {
                 //super.onInitializeAccessibilityNodeInfo(host, info);
-                info.setContentDescription("Current value: " + p.getValue());
+                info.setContentDescription("Current value: " + p1.getDisplayedValues()[p1.getValue()]);
             }
         });
 
-        final String[] focussedThing = {""};
-
-        p.setAccessibilityDelegate(new View.AccessibilityDelegate(){
-            @Override
-            public void sendAccessibilityEvent(View host, int eventType) {
-                Log.i("TG", "sendAccessibilityEvent: " + eventType);
-                super.sendAccessibilityEvent(host, eventType);
-            }
-
-            @Override
-            public boolean performAccessibilityAction(View host, int action, Bundle args) {
-                Log.i("TG", "performAccessibilityAction: " + action);
-                return super.performAccessibilityAction(host, action, args);
-                //return false;
-            }
-
-            @Override
-            public void sendAccessibilityEventUnchecked(View host, AccessibilityEvent event) {
-                Log.i("TG", "sendAccessibilityEventUnchecked: " + event.toString());
-                super.sendAccessibilityEventUnchecked(host, event);
-            }
-
-            @Override
-            public boolean dispatchPopulateAccessibilityEvent(View host, AccessibilityEvent event) {
-                Log.i("TG", "dispatchPopulateAccessibilityEvent: " + event.toString() );
-                return super.dispatchPopulateAccessibilityEvent(host, event);
-                //return false;
-            }
-
-            @Override
-            public void onPopulateAccessibilityEvent(View host, AccessibilityEvent event) {
-                Log.i("TG", "onPopulateAccessibilityEvent: " + event.toString());
-                super.onPopulateAccessibilityEvent(host, event);
-            }
-
-            @Override
-            public void onInitializeAccessibilityEvent(View host, AccessibilityEvent event) {
-                Log.i("TG", "onInitializeAccessibilityEvent: " + event.toString());
-                super.onInitializeAccessibilityEvent(host, event);
-            }
-
-            @Override
-            public void onInitializeAccessibilityNodeInfo(View host, AccessibilityNodeInfo info) {
-                Log.i("TG", "onInitializeAccessibilityNodeInfo: " +info.toString());
-                super.onInitializeAccessibilityNodeInfo(host, info);
-            }
-
-            @Override
-            public void addExtraDataToAccessibilityNodeInfo(@NonNull View host, @NonNull AccessibilityNodeInfo info, @NonNull String extraDataKey, @Nullable Bundle arguments) {
-                Log.i("TG", "addExtraDataToAccessibilityNodeInfo: " + info.toString());
-                super.addExtraDataToAccessibilityNodeInfo(host, info, extraDataKey, arguments);
-            }
-
+        p1.setAccessibilityDelegate(new View.AccessibilityDelegate(){
             @Override
             public boolean onRequestSendAccessibilityEvent(ViewGroup host, View child, AccessibilityEvent event) {
                 Log.i("TG", "onRequestSendAccessibilityEvent: " + event.toString());
                 if(event.getEventType() == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED){
-                    host.announceForAccessibility("Blah replaced blah.");
+                    String replacedText = event.getText().get(0) + " replaced " + event.getBeforeText() + ". ";
+                    int indexOfNew = Arrays.asList(p1.getDisplayedValues()).indexOf(event.getText().get(0).toString());
+
+                    if(indexOfNew == 0 && focussedThing[0].equals("before")){
+                        replacedText+="No previous values to choose from";
+                    } else if(indexOfNew == p1.getDisplayedValues().length -1 && focussedThing[0].equals("after")){
+                        replacedText+="No subsequent values to choose from";
+                    } else if(focussedThing[0].equals("before")){
+                        replacedText+=p1.getDisplayedValues()[indexOfNew -1] + ". Button.";
+                    } else if(focussedThing[0].equals("after")){
+                        replacedText+=p1.getDisplayedValues()[indexOfNew + 1] + ". Button.";
+                    } else if(focussedThing[0].equals("current")){
+                        replacedText+=p1.getDisplayedValues()[indexOfNew -1] + ". Current value: " + event.getText().get(0);
+                    }
+
+
+                    host.announceForAccessibility(replacedText);
                     return false;
                 }
 
                 if(event.getEventType() == AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED){
-                    Log.i("TG", event.getClassName().toString());
-                    Log.i("TG", event.getText().get(0).toString() + p.getValue());
-
                     if(event.getClassName() == "android.widget.EditText"){
                         focussedThing[0] = "current";
                     } else {
-                        int i = Integer.parseInt(event.getText().get(0).toString());
-                        if(i > p.getValue()){
-                            event.setContentDescription("Blah");
-                            host.announceForAccessibility("Tap to select next value in the picker");
+                        int i = Arrays.asList(p1.getDisplayedValues()).indexOf(event.getText().get(0).toString());
+                        if(i > p1.getValue()){
                             focussedThing[0] = "after";
-                            return true;
-                        } else if(i < p.getValue()) {
+                        } else if(i < p1.getValue()) {
                             host.announceForAccessibility("Tap to select the previous value in the picker");
                             focussedThing[0] = "before";
                         }
                     }
                 }
                 return super.onRequestSendAccessibilityEvent(host, child, event);
-            }
-
-            @Override
-            public AccessibilityNodeProvider getAccessibilityNodeProvider(View host) {
-                Log.i("TG", "getAccessibilityNodeProvider");
-                return super.getAccessibilityNodeProvider(host);
             }
         });
     }
